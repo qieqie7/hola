84d4ce4c832963c7c9125015f48c1eaf2b589960
ykst <ykst615@qq.com>
2019-12-29 20:42:45 +0800

    can run

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -1,7 +1,20 @@
+import path from 'path';
 import { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const config: Configuration = {
   mode: 'production',
+  entry: path.resolve(__dirname, '../src/index.ts'),
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'index.bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'webpack app',
+      template: 'src/index.html',
+    }),
+  ],
 };
 
 export default config;
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+console.log('Hello world');
+document.getElementById('root').innerHTML = 'hello world';
